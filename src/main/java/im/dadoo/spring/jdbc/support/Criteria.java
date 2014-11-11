52ddfa37e2b6c5e33e26cdba2b93f30acdea0455
codekitten <codekitten@qq.com>
2014-11-11 23:54:35 +0800

    init

---
+++ src/main/java/im/dadoo/spring/jdbc/support/Criteria.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties. To change this
+ * template file, choose Tools | Templates and open the template in the editor.
+ */
+package im.dadoo.spring.jdbc.support;
+
+import im.dadoo.spring.jdbc.support.condition.Condition;
+
+import java.util.List;
+
+/**
+ * 提供静态方法，用于生成供spring-jdbc使用的where或者orderBy子句
+ * 
+ * @author shuwen.zsw
+ */
+public final class Criteria {
+  
+  private Criteria() {}
+
+  /**
+   * 根据所提供的conditions，动态生成where的查询子句
+   * 
+   * @param conditions 动态查询条件
+   * @return 查询sql字符串
+   */
+  public static final String where(final List<Condition> conditions) {
+    StringBuilder sb = new StringBuilder("");
+    if (conditions != null && !conditions.isEmpty()) {
+      Condition condition = conditions.get(0);
+      if (condition != null) {
+        sb.append(" WHERE ").append(makeConditionSql(condition));
+        for (int i = 1; i < conditions.size(); i++) {
+          sb.append(" AND ").append(makeConditionSql(conditions.get(i)));
+        }
+      }
+    }
+    return sb.toString();
+  }
+  
+  /**
+   * 根据所提供的orders，生成order by的查询子句
+   * 
+   * @param orders 动态排序条件
+   * @return 生成的order by字符串
+   */
+  public static String orderBy(final List<Pair<String, String>> orders) {
+    StringBuilder sb = new StringBuilder();
+    if (orders != null && !orders.isEmpty()) {
+      sb.append("ORDER BY ");
+      Pair<String, String> first = orders.get(0);
+      sb.append(first.getV1()).append(" ").append(first.getV2());
+      for (int i = 1; i < orders.size(); i++) {
+        Pair<String, String> order = orders.get(i);
+        sb.append(",").append(order.getV1()).append(" ").append(order.getV2());
+      }
+    }
+    return sb.toString();
+  }
+  
+  private static String makeConditionSql(Condition condition) {
+    StringBuilder sb = new StringBuilder();
+    if (condition != null && condition.getField() != null && condition.getOp() != null
+        && condition.getValue() != null) {
+      switch (condition.getOp()) {
+        case EQ:
+        case NE:
+        case GT:
+        case GE:
+        case LT:
+        case LE:
+        case LIKE:
+          sb.append(condition.getField()).append(" ").append(condition.getOp().getName())
+              .append(" ").append(condition.getValue());
+          break;
+        case BETWEEN:
+          @SuppressWarnings("unchecked")
+          Pair<String, String> pair = (Pair<String, String>) condition.getValue();
+          sb.append(condition.getField()).append(" ").append(condition.getOp().getName())
+              .append(" ").append(pair.getV1()).append(" ").append("AND").append(" ")
+              .append(pair.getV2());
+          break;
+        case IN:
+          sb.append(condition.getField()).append(" ").append(condition.getOp().getName())
+              .append(" ").append("( ").append(condition.getValue()).append(" )");
+          break;
+      }
+    }
+    return sb.toString();
+  }
+  
+}
---
+++ src/test/java/im/dadoo/spring/jdbc/support/test/CriteriaTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package im.dadoo.spring.jdbc.support.test;
+
+import im.dadoo.spring.jdbc.support.Criteria;
+import im.dadoo.spring.jdbc.support.Pair;
+import im.dadoo.spring.jdbc.support.condition.Condition;
+import im.dadoo.spring.jdbc.support.condition.Conditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author shuwen.zsw
+ */
+public class CriteriaTest {
+  
+  @Test
+  public void testWhere() {
+    List<Condition> conds = new ArrayList<Condition>();
+    conds.add(Conditions.eq("id"));
+    conds.add(Conditions.between("utc"));
+    conds.add(Conditions.in("name"));
+    System.out.println(Criteria.where(conds));
+  }
+  
+  @Test
+  public void testOrderBy() {
+    List<Pair<String, String>> orders = new ArrayList<>();
+    orders.add(Pair.of("name", "DESC"));
+    orders.add(Pair.of("id", "ASC"));
+    System.out.println(Criteria.orderBy(orders));
+  }
+  
+}
